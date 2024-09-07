32827f88f6e9eee1b528809e2402e4e7aebf5483
warnerfleming <fleming14@usf.edu>
2024-09-07 16:25:01 -0400

    Implement and populate arrays for bills, tips, and totals

--- cc_2.js
+++ cc_2.js
@@ -12,10 +12,19 @@ console.log( `The bill was $${x}, the tip was $${tip}, and the totl value is $${
 
 // Task 3
 
-function calculateTip (billA,tip) {
-        return (50 <= billA && billA <= 300) ? (billA * .15) : (billA * .20)
+function calculateTip (bills,tip) {
+        return (50 <= bills && bills <= 300) ? (bills * .15) : (bills * .20)
 }
 
-let billA = 100
-let newbill = calculateTip (billA)
+ let bills = 100
+let newbill = calculateTip (bills)
 console.log(newbill)
+
+
+// Task 4
+bills =[275,40,430]
+const tips = bills.map(bills => calculateTip(bills)) 
+console.log("Correspinding tips for bills of $275, $40, and $430 :", tips) 
+
+let totals = bills.map(bills => calculateTip(bills) + bills)
+console.log(`The totals of each bill with tips are ${totals}`)
