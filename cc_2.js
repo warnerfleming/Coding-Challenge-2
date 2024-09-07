fc889be377e006406469fbda0109152c7c45e792
warnerfleming <fleming14@usf.edu>
2024-09-07 15:08:01 -0400

    Create and test calculateTip function

--- cc_2.js
+++ cc_2.js
@@ -1,7 +1,7 @@
 //Task 1
 
 let x = 70
-let tip = (50 <= x <= 300) ? (x * .15) : (x * .20)
+let tip = (50 <=x && x <= 300) ? (x * .15) : (x * .20)
 console.log(tip)
 
 
@@ -9,3 +9,13 @@ console.log(tip)
 
 let total = x + tip
 console.log( `The bill was $${x}, the tip was $${tip}, and the totl value is $${total}`)
+
+// Task 3
+
+function calculateTip (billA,tip) {
+        return (50 <= billA && billA <= 300) ? (billA * .15) : (billA * .20)
+}
+
+let billA = 100
+let newbill = calculateTip (billA)
+console.log(newbill)
